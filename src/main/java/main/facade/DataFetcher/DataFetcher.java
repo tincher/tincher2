937cf9a8a7c0314d8ec2673f765b697e08f12ae9
tincher <joel.ewig@gmail.com>
2017-06-21 00:56:58 +0200

    update profile img

--- src/main/java/main/facade/DataFetcher/DataFetcher.java
+++ src/main/java/main/facade/DataFetcher/DataFetcher.java
@@ -11,17 +11,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 /**
  * Created by Joel on 11.06.2017.
  */
+
+@Service
 public class DataFetcher {
     private final String URL = "https://playoverwatch.com/de-de/career/pc/eu/";
     private final String URL_SEARCH = "https://playoverwatch.com/de-de/search?q=";
-
-
 
 
     public Profile fetchForNewUser(Registration registration) throws IOException {
@@ -29,11 +30,10 @@ public class DataFetcher {
         Profile result = new Profile(registration);
 
         //JSOUP part
-        Document doc = null;
-        doc = Jsoup.connect(URL + registration.getUsername() + "-" + String.valueOf(registration.getBnt())).timeout(20 * 1000).get();
+        Document doc = getDocument(registration.getBnt(), registration.getUsername());
         Element champstats = doc.getElementById("competitive");
 
-        result.getHeadUpProfile().setProfileImgUrl(doc.getElementsByClass("player-portrait").get(0).attr("src"));
+        result.getHeadUpProfile().setProfileImgUrl(getProfileImage(registration.getBnt(), registration.getUsername()));
 
         if (champstats != null && champstats.getElementsByClass("career-stats-section") != null) {
 
@@ -42,50 +42,62 @@ public class DataFetcher {
 
             //Init PlayedChamps
             PlayedChamps playedChamps = new PlayedChamps();
+            if (dataTableElems.size() > 0) {
 
-            for (int i = 1; i < dataTableElems.get(0).getElementsByTag("option").size(); i++) {
-                Champion champion = new Champion();
-                champion.setName(dataTableElems.get(0).getElementsByTag("option").get(i).text());
-                playedChamps.addToChampList(champion);
-            }
-
-            //remove select item and overall stats
-            dataTableElems.remove(0);
-            dataTableElems.remove(0);
-
-            for (Element elem : dataTableElems) {
-
-                //Init Champstats
-                ChampionStats championStats = new ChampionStats();
-                Elements blockStats = elem.getElementsByTag("tbody");
-
-                int i = 0;
-                for (Element stat : blockStats) {
-                    //Init ChampStats
-                    StatBlock statBlock = new StatBlock();
-                    statBlock.setBlockname(elem.getElementsByClass("stat-title").get(i).text());
-
-                    //iter through statcard
-                    int j = 0, k = 1;
-                    while (stat.getElementsByTag("td").size() >= k) {
-                        statBlock.addValueToMap(stat.getElementsByTag("td").get(j).text(), stat.getElementsByTag("td").get(k).text());
-                        j += 2;
-                        k += 2;
-                    }
-                    championStats.addStatBlock(statBlock);
-                    i++;
+                for (int i = 1; i < dataTableElems.get(0).getElementsByTag("option").size(); i++) {
+                    Champion champion = new Champion();
+                    champion.setName(dataTableElems.get(0).getElementsByTag("option").get(i).text());
+                    playedChamps.addToChampList(champion);
                 }
 
-                //set statblocks to fitting champ
-                for (Champion champion : playedChamps.getChampionList()) {
-                    if (champion.getChampionStats() == null) {
-                        champion.setChampionStats(championStats);
-                        break;
+                //remove select item and overall stats
+                dataTableElems.remove(0);
+                dataTableElems.remove(0);
+
+                for (Element elem : dataTableElems) {
+
+                    //Init Champstats
+                    ChampionStats championStats = new ChampionStats();
+                    Elements blockStats = elem.getElementsByTag("tbody");
+
+                    int i = 0;
+                    for (Element stat : blockStats) {
+                        //Init ChampStats
+                        StatBlock statBlock = new StatBlock();
+                        statBlock.setBlockname(elem.getElementsByClass("stat-title").get(i).text());
+
+                        //iter through statcard
+                        int j = 0, k = 1;
+                        while (stat.getElementsByTag("td").size() >= k) {
+                            statBlock.addValueToMap(stat.getElementsByTag("td").get(j).text(), stat.getElementsByTag("td").get(k).text());
+                            j += 2;
+                            k += 2;
+                        }
+                        championStats.addStatBlock(statBlock);
+                        i++;
+                    }
+
+                    //set statblocks to fitting champ
+                    for (Champion champion : playedChamps.getChampionList()) {
+                        if (champion.getChampionStats() == null) {
+                            champion.setChampionStats(championStats);
+                            break;
+                        }
                     }
                 }
+                result.getHeadUpProfile().setPlayedChamps(playedChamps);
             }
-            result.getHeadUpProfile().setPlayedChamps(playedChamps);
         }
         return result;
+    }
+
+    private Document getDocument(int bnt, String username) throws IOException {
+        Document doc = null;
+        doc = Jsoup.connect(URL + username + "-" + String.valueOf(bnt)).timeout(20 * 1000).get();
+        return doc;
+    }
+
+    public String getProfileImage(int bnt, String username) throws IOException {
+        return getDocument(bnt, username).getElementsByClass("player-portrait").get(0).attr("src");
     }
 }
--- src/main/java/main/facade/RESTController/ProfileRestController.java
+++ src/main/java/main/facade/RESTController/ProfileRestController.java
@@ -1,6 +1,7 @@
 package main.facade.RESTController;
 
 import main.domain.user.profile.Profile;
+import main.facade.DataFetcher.DataFetcher;
 import main.facade.Services.ProfileService;
 import main.persistence.user.profile.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.NonUniqueResultException;
+import javax.xml.crypto.Data;
+import java.io.IOException;
 
 /**
  * Created by Joel on 15.06.2017.
@@ -24,6 +27,9 @@ public class ProfileRestController {
     @Autowired
     private ProfileService profileService;
 
+    @Autowired
+    private DataFetcher dataFetcher;
+
 
     @RequestMapping(value = "/{bnt}/{username}", method = RequestMethod.GET)
     public Profile getProfile(@PathVariable("bnt") int bnt, @PathVariable("username") String username) {
@@ -31,8 +37,15 @@ public class ProfileRestController {
             Profile result = profileService.getProfile(bnt, username);
             return result;
         } catch (NonUniqueResultException e) {
-            return  null;
+            return null;
         }
+    }
+
+
+    @RequestMapping(value = "/refreshProfileImage/{bnt}/{username}")
+    public void refreshProfileImage(@PathVariable("bnt") int bnt, @PathVariable("username") String username) throws IOException {
+        Profile profile = getProfile(bnt, username);
+        profile.getHeadUpProfile().setProfileImgUrl(dataFetcher.getProfileImage(bnt, username));
     }
 
 
