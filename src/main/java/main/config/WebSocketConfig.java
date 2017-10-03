c97d28ce7ae61a7b0eec897ab0cd2ba315f611b2
h3xt3k <jojoewig@web.de>
2017-10-03 16:41:53 +0200

    proxy#

--- src/main/java/main/config/WebSocketConfig.java
+++ src/main/java/main/config/WebSocketConfig.java
@@ -1,27 +1,27 @@
-package main.config;
+// package main.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-/**
- * Created by Joel on 07.07.2017.
- */
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+// /**
+//  * Created by Joel on 07.07.2017.
+//  */
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry config) {
+//         config.enableSimpleBroker("/topic");
+//         config.setApplicationDestinationPrefixes("/app");
+//     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
-    }
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         registry.addEndpoint("/gs-guide-websocket").withSockJS();
+//     }
 
-}
+// }
